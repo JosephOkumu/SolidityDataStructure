dfcc5196001a9a951481de0c969c3069f13497f4
JosephOkumu <jokumu25@gmail.com>
2025-05-04 22:32:06 +0300

    feat: add enum status

---
+++ Todo.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract TodoList {
+    // 1. Todo Status Enum
+    enum Status {
+        Pending,
+        InProgress,
+        Completed
+    }
+
+   
